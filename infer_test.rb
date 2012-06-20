ff204fe964200a0d75d14382db059c20796c5964
snipsnipsnip <snipsnipsnip@users.sourceforge.jp>
2012-06-20 14:48:13 +0900

    back to shoulda

--- infer_test.rb
+++ infer_test.rb
@@ -1,9 +1,10 @@
 require 'infer'
-require 'minitest/autorun'
+require 'rubygems'
+require 'shoulda'
 
-describe Infer do
-  let :default_env do
-    {
+class InferTest < Test::Unit::TestCase
+  def default_env
+    @default_env ||= {
       :cons => [:num, [:list, :list]],
       :car => [:list, :list],
       :cdr => [:list, :list],
@@ -21,122 +22,125 @@ describe Infer do
     yield
     @verbose = false    
   end
-  
-  describe "vars" do
-    it "should be typed as in context" do
-      default_env.each do |k,v|
-        assert_equal v, infer(k)
+    
+  context "mono" do
+    context "vars" do
+      should "be typed as in context" do
+        default_env.each do |k,v|
+          assert_equal v, infer(k)
+        end
       end
     end
-  end
 
-  describe "numbers" do
-    it "should be typed as num" do
-      assert_equal :num, infer(3)
-    end
-  end
-  
-  describe "app" do
-    it "should be typed from result" do
-      assert_equal :list, infer([:car, :nil])
-      assert_equal :list, infer([:cdr, :nil])
-      assert_equal :bool, infer([:null, :nil])
-      assert_equal [:list, :list], infer([:cons, 1])
-      assert_equal :list, infer([:cons, 1, :nil])
-    end
-    
-    it "should raise error on arg type mismatch" do
-      assert_raises(Infer::TypeMismatchError) { infer([:car, 1]) }
-      assert_raises(Infer::TypeMismatchError) { infer([:cdr, 1]) }
-      assert_raises(Infer::TypeMismatchError) { infer([:null, 1]) }
-    end
-    
-    it "should detect non-function app" do
-      assert_raises(Infer::TypeMismatchError) { infer([:nil, 1]) }
-      assert_raises(Infer::TypeMismatchError) { infer([:nil, :car]) }
-      assert_raises(Infer::TypeMismatchError) { infer([4, :car]) }
-      assert_raises(Infer::TypeMismatchError) { infer([:null, :nil, :nil]) }
-      assert_raises(Infer::TypeMismatchError) { infer([:cons, :nil, :nil, :nil]) }
-    end
-    
-    it "shouldn't be able to type higher-rank stuff" do
-      assert_raises(Infer::TypeMismatchError) { infer([:cons, :nil]) }
-      assert_raises(Infer::TypeMismatchError) { infer([:cons, :cons]) }
-    end
-  end
-  
-  describe "abs" do
-    it "should type constant function" do
-      assert_equal [:a, :num], infer([:^, :x, 1])
-      default_env.each do |k,v|
-        assert_equal [:a, v], infer([:^, :x, k])
+    context "numbers" do
+      should "be typed as num" do
+        assert_equal :num, infer(3)
       end
     end
     
-    it "should type some combinators" do
-      assert_equal [:a, :a], infer([:^, :x, :x])
-      assert_equal [:a, [:b, :a]], infer([:^, :x, [:^, :y, :x]])
-      assert_equal [:a, [:b, :b]], infer([:^, :x, [:^, :y, :y]])
-      assert_equal [:a, [:b, :num]], infer([:^, :x, [:^, :y, 3]])
-      assert_equal [[:a, [:b, :c]], [[:a, :b], [:a, :c]]], infer([:^, :x, [:^, :y, [:^, :z, [[:x, :z], [:y, :z]]]]])
+    context "app" do
+      should "be typed from result" do
+        assert_equal :list, infer([:car, :nil])
+        assert_equal :list, infer([:cdr, :nil])
+        assert_equal :bool, infer([:null, :nil])
+        assert_equal [:list, :list], infer([:cons, 1])
+        assert_equal :list, infer([:cons, 1, :nil])
+      end
       
-      assert_equal [:a, :a], infer([
-        [:^, :x, [:^, :y, [:^, :z, [[:x, :z], [:y, :z]]]]],
-        [:^, :x, [:^, :y, :x]],
-        [:^, :x, [:^, :y, :x]]
-      ])
-    end
-    
-    it "should handle some complex expressions" do
-      assert_equal :num, infer([[:^, :x, :x], 3])
-      assert_equal [:a, :a], infer([[:^, :x, :x], [:^, :x, :x]])
-      assert_equal [:num, [:list, :list]], infer([[:^, :x, :x], :cons])
-      assert_equal :list, infer([[:^, :x, :x], :cons, 10, :nil])
-      assert_equal [:a, :a], infer([[:^, :x, :x], [:^, :x, :x], [:^, :x, :x]])
-      assert_equal [:a, [:b, [[:b, :c], [:d, :c]]]], infer([:^, :a, [:^, :b, [[:^, :c, [:^, :d, [:^, :e, [:d, :b]]]], [:^, :f, [:^, :g, :f]]]]])
-      assert_equal [:a, [:b, [:c, :b]]], infer([:^, :a, [[:^, :b, [:^, :c, [:^, :d, :c]]], :a]])
-      assert_equal [:a, [[:b, :c], [[[:d, [:e, [[[:b, :c], [:c, :f]], :f]]], :g], [:b, :g]]]], infer([:^, :a, [:^, :b, [:^, :c, [:^, :d, [:c, [:^, :e, [:^, :f, [:^, :g, [[:g, :b], [:b, :d]]]]]]]]]])
-    end
-    
-    it "should induct" do
-      assert_equal [:list, :bool], infer([:^, :x, [:null, :x]])
-      assert_equal :bool, infer([[:^, :x, [:null, :x]], :nil])
-      assert_equal [:list, :list], infer([:^, :x, [:car, :x]])
-      assert_equal [:list, :list], infer([:^, :x, [:cdr, :x]])
-      assert_equal [:num, [:list, :list]], infer([:^, :x, [:cons, :x]])
-      assert_equal [:num, [:list, :list]], infer([:^, :x, [:^, :y, [:cons, :x, :y]]])
-      assert_equal [:list, [:num, :list]], infer([:^, :x, [:^, :y, [:cons, :y, :x]]])
-      assert_equal [[:a, :b], [:a, :b]], infer([:^, :x, [:^, :y, [:x, :y]]])
-      assert_equal [:a, [[:a, :b], :b]], infer([:^, :x, [:^, :y, [:y, :x]]])
-    end
-    
-    it "should deduct" do
-      assert_equal [[:num, :a], :a], infer([:^, :x, [:x, 3]])
-      assert_equal [[[:list, :list], :a], :a], infer([:^, :x, [:x, :car]])
-      assert_equal [[:num, [:num, :a]], :a], infer([:^, :x, [:x, 3, 4]])
-      assert_equal [[:num, [:list, :a]], :a], infer([:^, :x, [:x, 3, :nil]])
-      assert_equal [[:list, [:num, :a]], :a], infer([:^, :x, [:x, :nil, 3]])
-    end
-    
-    it "should detect mismatch" do
-      assert_raises(Infer::TypeMismatchError) { infer([:^, :x, [:nil, :x]]) }
-      assert_raises(Infer::TypeMismatchError) do
-        infer([:s, :i, :i], {
-          :i => [:num, :num],
-          :s => [[:num, :num, :num], [[:num, :num], [:num, :num]]]
-        })
+      should "raise error on arg type mismatch" do
+        assert_raises(Infer::TypeMismatchError) { infer([:car, 1]) }
+        assert_raises(Infer::TypeMismatchError) { infer([:cdr, 1]) }
+        assert_raises(Infer::TypeMismatchError) { infer([:null, 1]) }
+      end
+      
+      should "detect non-function app" do
+        assert_raises(Infer::TypeMismatchError) { infer([:nil, 1]) }
+        assert_raises(Infer::TypeMismatchError) { infer([:nil, :car]) }
+        assert_raises(Infer::TypeMismatchError) { infer([4, :car]) }
+        assert_raises(Infer::TypeMismatchError) { infer([:null, :nil, :nil]) }
+        assert_raises(Infer::TypeMismatchError) { infer([:cons, :nil, :nil, :nil]) }
+      end
+      
+      should "shouldn't be able to type higher-rank stuff" do
+        assert_raises(Infer::TypeMismatchError) { infer([:cons, :nil]) }
+        assert_raises(Infer::TypeMismatchError) { infer([:cons, :cons]) }
       end
     end
     
-    it "should detect loop" do
-      assert_raises(Infer::LoopError) { infer([:^, :x, [:x, :x]]) }
-      assert_raises(Infer::LoopError) { infer([:^, :x, [:x, :x, :x]]) }
-      assert_raises(Infer::LoopError) { infer([:^, :x, [:x, :x, :x, :x]]) }
-      assert_raises(Infer::LoopError) { infer([:^, :x, [:^, :y, [:x, :x]]]) }
-      assert_raises(Infer::LoopError) { infer([:^, :x, [:^, :y, [:y, :x, :y]]]) }
+    context "abs" do
+      should "type constant function" do
+        assert_equal [:a, :num], infer([:^, :x, 1])
+        default_env.each do |k,v|
+          assert_equal [:a, v], infer([:^, :x, k])
+        end
+      end
       
-      assert_raises(Infer::LoopError) do
-        infer [:^, :a, [[[:^, :b, [:^, :c, :a]], :a], [[:a, [[[:^, :d, :d], [:a, :a]], [:^, :e, [:^, :f, :f]]]], [:^, :g, :g]]]]
+      should "type some combinators" do
+        assert_equal [:a, :a], infer([:^, :x, :x])
+        assert_equal [:a, [:b, :a]], infer([:^, :x, [:^, :y, :x]])
+        assert_equal [:a, [:b, :b]], infer([:^, :x, [:^, :y, :y]])
+        assert_equal [:a, [:b, :num]], infer([:^, :x, [:^, :y, 3]])
+        assert_equal [[:a, [:b, :c]], [[:a, :b], [:a, :c]]], infer([:^, :x, [:^, :y, [:^, :z, [[:x, :z], [:y, :z]]]]])
+        
+        assert_equal [:a, :a], infer([
+          [:^, :x, [:^, :y, [:^, :z, [[:x, :z], [:y, :z]]]]],
+          [:^, :x, [:^, :y, :x]],
+          [:^, :x, [:^, :y, :x]]
+        ])
+      end
+      
+      should "handle some complex expressions" do
+        assert_equal :num, infer([[:^, :x, :x], 3])
+        assert_equal [:a, :a], infer([[:^, :x, :x], [:^, :x, :x]])
+        assert_equal [:num, [:list, :list]], infer([[:^, :x, :x], :cons])
+        assert_equal :list, infer([[:^, :x, :x], :cons, 10, :nil])
+        assert_equal [:a, :a], infer([[:^, :x, :x], [:^, :x, :x], [:^, :x, :x]])
+        assert_equal [:a, [:b, [[:b, :c], [:d, :c]]]], infer([:^, :a, [:^, :b, [[:^, :c, [:^, :d, [:^, :e, [:d, :b]]]], [:^, :f, [:^, :g, :f]]]]])
+        assert_equal [:a, [:b, [:c, :b]]], infer([:^, :a, [[:^, :b, [:^, :c, [:^, :d, :c]]], :a]])
+        assert_equal [:a, [[:b, :c], [[[:d, [:e, [[[:b, :c], [:c, :f]], :f]]], :g], [:b, :g]]]], infer([:^, :a, [:^, :b, [:^, :c, [:^, :d, [:c, [:^, :e, [:^, :f, [:^, :g, [[:g, :b], [:b, :d]]]]]]]]]])
+      end
+      
+      should "induct" do
+        assert_equal [:list, :bool], infer([:^, :x, [:null, :x]])
+        assert_equal :bool, infer([[:^, :x, [:null, :x]], :nil])
+        assert_equal [:list, :list], infer([:^, :x, [:car, :x]])
+        assert_equal [:list, :list], infer([:^, :x, [:cdr, :x]])
+        assert_equal [:num, [:list, :list]], infer([:^, :x, [:cons, :x]])
+        assert_equal [:num, [:list, :list]], infer([:^, :x, [:^, :y, [:cons, :x, :y]]])
+        assert_equal [:list, [:num, :list]], infer([:^, :x, [:^, :y, [:cons, :y, :x]]])
+        assert_equal [[:a, :b], [:a, :b]], infer([:^, :x, [:^, :y, [:x, :y]]])
+        assert_equal [:a, [[:a, :b], :b]], infer([:^, :x, [:^, :y, [:y, :x]]])
+      end
+      
+      should "deduct" do
+        assert_equal [[:num, :a], :a], infer([:^, :x, [:x, 3]])
+        assert_equal [[[:list, :list], :a], :a], infer([:^, :x, [:x, :car]])
+        assert_equal [[:num, [:num, :a]], :a], infer([:^, :x, [:x, 3, 4]])
+        assert_equal [[:num, [:list, :a]], :a], infer([:^, :x, [:x, 3, :nil]])
+        assert_equal [[:list, [:num, :a]], :a], infer([:^, :x, [:x, :nil, 3]])
+      end
+      
+      should "detect mismatch" do
+        assert_raises(Infer::TypeMismatchError) { infer([:^, :x, [:nil, :x]]) }
+        assert_raises(Infer::TypeMismatchError) do
+          infer([:s, :i, :i], {
+            :i => [:num, :num],
+            :s => [[:num, :num, :num], [[:num, :num], [:num, :num]]]
+          })
+        end
+      end
+      
+      should "detect loop" do
+        assert_raises(Infer::LoopError) { infer([:^, :x, [:x, :x]]) }
+        assert_raises(Infer::LoopError) { infer([:^, :x, [:x, :x, :x]]) }
+        assert_raises(Infer::LoopError) { infer([:^, :x, [:x, :x, :x, :x]]) }
+        assert_raises(Infer::LoopError) { infer([:^, :x, [:^, :y, [:x, :x]]]) }
+        assert_raises(Infer::LoopError) { infer([:^, :x, [:^, :y, [:y, :x, :y]]]) }
+        assert_raises(Infer::LoopError) { infer([:^, :f, [[:^, :x, [:f, [:x, :x]]], [:^, :x, [:f, [:x, :x]]]]]) }
+        
+        assert_raises(Infer::LoopError) do
+          infer [:^, :a, [[[:^, :b, [:^, :c, :a]], :a], [[:a, [[[:^, :d, :d], [:a, :a]], [:^, :e, [:^, :f, :f]]]], [:^, :g, :g]]]]
+        end
       end
     end
   end
